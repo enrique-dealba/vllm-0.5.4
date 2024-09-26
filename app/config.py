9e85d60ddbdff4479b15f96e382c09674b540088
enrique-dealba <140011964+enrique-dealba@users.noreply.github.com>
2024-09-26 13:52:10 -0500

    fix: img url

--- app/config.py
+++ app/config.py
@@ -3,8 +3,8 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     MODEL_NAME: str = "llava-hf/llava-1.5-7b-hf"
-    # FIXED_IMAGE_URL: str = "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg"
-    FIXED_IMAGE_URL: str = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/The_station_pictured_from_the_SpaceX_Crew_Dragon_1.jpg/1920px-The_station_pictured_from_the_SpaceX_Crew_Dragon_1.jpg"
+    FIXED_IMAGE_URL: str = "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg"
+    # FIXED_IMAGE_URL: str = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/The_station_pictured_from_the_SpaceX_Crew_Dragon_1.jpg/1920px-The_station_pictured_from_the_SpaceX_Crew_Dragon_1.jpg"
     IMAGE_FETCH_TIMEOUT: int = 5
     PORT: int = 8888
     HOST: str = "0.0.0.0"
