37be8c8935a87560382175dac46e2c406fc352ba
enrique-dealba <140011964+enrique-dealba@users.noreply.github.com>
2024-11-14 16:12:41 -0600

    fix: test docker compose

--- run_tests.sh
+++ run_tests.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Stop any existing containers
-docker-compose -f docker-compose.test.yml down -v
+docker compose -f docker-compose.test.yml down -v
 
 # Build and run tests
-docker-compose -f docker-compose.test.yml build
-docker-compose -f docker-compose.test.yml run tests
+docker compose -f docker-compose.test.yml build
+docker compose -f docker-compose.test.yml run tests
 
 # Cleanup
-docker-compose -f docker-compose.test.yml down -v
+docker compose -f docker-compose.test.yml down -v
