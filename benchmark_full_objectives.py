fa56c0a25283edeed4663dfad1d155010495b2dd
enrique-dealba <140011964+enrique-dealba@users.noreply.github.com>
2025-02-05 14:04:45 -0600

    debug: obj types

--- benchmark_full_objectives.py
+++ benchmark_full_objectives.py
@@ -13,8 +13,8 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 # Benchmark settings
-N_ITERATIONS = 3  # Number of times to run each test case
-RATE_LIMIT_DELAY = 3.0  # Seconds to wait between requests
+N_ITERATIONS = 5  # Number of times to run each test case
+RATE_LIMIT_DELAY = 15.0  # Seconds to wait between requests
 MAX_CONCURRENT_REQUESTS = 1  # Maximum number of concurrent requests
 
 # ------------------------------------------------------------------------------
